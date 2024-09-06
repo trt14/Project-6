c07fd45c57d90b3e040981947babd6024bcc595e
Ali_Altarouty <tarooti14dev@gmail.com>
2024-09-06 07:38:36 +0300

    initialize the main folders and main files

---
+++ my_products_app/README.md
@@ -0,0 +1,3 @@
+# my_products_app
+
+A new Flutter project.
---
+++ my_products_app/android/app/src/main/kotlin/com/example/my_products_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_products_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
